21a9fefa19d7e786ea0dfc2b5651a5639d06e9df
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2026-06-15 00:44:33 -0700

    Update gradle and lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     id("com.gradleup.shadow") version "9.3.1"
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.19"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.21"
 }
 
 group = "io.github.lumine1909"
@@ -16,7 +16,7 @@ repositories {
 
 dependencies {
     paperweight.paperDevBundle("1.21.11-R0.1-SNAPSHOT")
-    implementation("io.github.lumine1909:reflexion:0.5.0")
+    implementation("io.github.lumine1909:reflexion:0.5.1")
 }
 
 tasks.shadowJar {
